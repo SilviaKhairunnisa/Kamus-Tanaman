aac4d8fb3006bcf1ddd1de52d9986fa5a105db66
SilviaKhairunnisa <silviakhairunnisa0503@gmail.com>
2025-06-23 00:56:17 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+const kamusData = [
+  {
+    "id": 1,
+    "judul": "bawang merah",
+    "pelafalan": "/ba·wang me·rah/",
+    "deskripsi": "umbi lapis berwarna merah sebagai bumbu masak.",
+    "manfaat": "Untuk mengobati flu dan batuk."
+  },
+  {
+    "id": 2,
+    "judul": "bawang putih",
+    "pelafalan": "/ba·wang pu·tih/",
+    "deskripsi": "umbi lapis berwarna putih sebagai bumbu masak.",
+    "manfaat": "Untuk meningkatkan sistem kekebalan tubuh."
+  },
+  {
+    "id": 3,
+    "judul": "belimbing wuluh",
+    "pelafalan": "/be·lim·bing wu·luh/",
+    "deskripsi": "buah berasa asam berbentuk bintang.",
+    "manfaat": "Untuk menurunkan tekanan darah tinggi."
+  },
+  {
+    "id": 4,
+    "judul": "bunga telang",
+    "pelafalan": "/bu•nga te•lang/",
+    "deskripsi": "bunga berwarna biru dari tanaman Clitoria ternatea.",
+    "manfaat": "Untuk pewarna makanan alami dan meningkatkan penglihatan."
+  },
+  {
+    "id": 5,
+    "judul": "cabe jawa",
+    "pelafalan": "/ca·be ja·wa/",
+    "deskripsi": "rempah berbentuk bulat panjang.",
+    "manfaat": "Untuk meningkatkan stamina."
+  },
+  {
+    "id": 6,
+    "judul": "cengkeh",
+    "pelafalan": "/ceng·keh/",
+    "deskripsi": "rempah berbentuk seperti paku.",
+    "manfaat": "Untuk meredakan sakit gigi."
+  },
+  {
+    "id": 7,
+    "judul": "serealia",
+    "pelafalan": "/se·re·a·li·a/",
+    "deskripsi": "buah bulat kecil rasa asam.",
+    "manfaat": "Untuk mengobati sariawan dan meningkatkan nafsu makan."
+  },
+  {
+    "id": 8,
+    "judul": "pohon cendana",
+    "pelafalan": "/sen•da•na/",
+    "deskripsi": "kayu beraroma dari pohon Santalum album.",
+    "manfaat": "Untuk aromaterapi dan pengobatan kulit."
+  },
+  {
+    "id": 9,
+    "judul": "daun binahong",
+    "pelafalan": "/da·un bi·na·hong/",
+    "deskripsi": "daun dari tanaman merambat.",
+    "manfaat": "Untuk mempercepat penyembuhan luka."
+  },
+  {
+    "id": 10,
+    "judul": "daun jambu biji",
+    "pelafalan": "/da·un jam·bu bi·ji/",
+    "deskripsi": "daun dari pohon jambu biji.",
+    "manfaat": "Untuk mengobati diare."
+  }
+];
